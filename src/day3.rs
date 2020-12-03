a6f30733008343de75d508184d98d4990feb7460
Dan Nguyen <pedantic@gmail.com>
2020-12-03 09:41:39 -0600

    Update day 2, refactor

--- src/day3.rs
+++ src/day3.rs
@@ -1,57 +1,63 @@
-pub struct Map {
+pub struct Forest {
     field: Vec<Vec<bool>>,
     width: usize,
+    height: usize,
 }
 
-impl Map {
-    fn get(&self, r: usize, c: usize) -> bool {
-        // let c = c % self.width;
+impl Forest {
+    fn new(field: Vec<Vec<bool>>) -> Self {
+        let width = field[0].len();
+        let height = field.len();
 
+        Self {
+            field,
+            width,
+            height,
+        }
+    }
+
+    fn get(&self, c: usize, r: usize) -> bool {
         self.field[r][c]
+    }
+
+    fn count_trees(&self, c_inc: usize, r_inc: usize) -> usize {
+        let mut c = 0;
+
+        (0..self.height)
+            .step_by(r_inc)
+            .filter(|&r| {
+                let c_orig = c;
+                c += c_inc;
+                if c >= self.width {
+                    c -= self.width;
+                }
+
+                self.get(c_orig, r)
+            })
+            .count()
     }
 }
 
 #[aoc_generator(day3)]
-pub fn day3_generator(input: &str) -> Map {
-    let field: Vec<Vec<_>> = input
-        .lines()
-        .map(|l| l.chars().map(|c| c == '#').collect())
-        .collect();
-
-    let width = field[0].len();
-
-    Map { field, width }
-}
-
-pub fn count_trees(inputs: &Map, c_inc: usize, r_inc: usize) -> usize {
-    let height = inputs.field.len();
-    let mut c = 0;
-
-    (0..height)
-        .step_by(r_inc)
-        .filter(|&r| {
-            let c_orig = c;
-            c += c_inc;
-            if c >= inputs.width {
-                c -= inputs.width;
-            }
-            inputs.get(r, c_orig)
-        })
-        .count()
+pub fn day3_generator(input: &str) -> Forest {
+    Forest::new(
+        input
+            .lines()
+            .map(|l| l.chars().map(|c| c == '#').collect())
+            .collect(),
+    )
 }
 
 #[aoc(day3, part1)]
-pub fn part1(inputs: &Map) -> usize {
-    count_trees(inputs, 3, 1)
+pub fn part1(inputs: &Forest) -> usize {
+    inputs.count_trees(3, 1)
 }
 
 #[aoc(day3, part2)]
-pub fn part2(inputs: &Map) -> usize {
-    let slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
-
-    slopes
+pub fn part2(inputs: &Forest) -> usize {
+    [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
         .iter()
-        .map(|(c, r)| count_trees(inputs, *c, *r))
+        .map(|&(c, r)| inputs.count_trees(c, r))
         .product()
 }
 
