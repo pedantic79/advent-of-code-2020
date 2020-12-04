6b86274477769aa5ef0fde8436074db4338a2f70
Dan Nguyen <pedantic@gmail.com>
2020-12-03 22:04:01 -0600

    Rename filles to two digit file names

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,10 @@ extern crate aoc_runner;
 #[macro_use]
 extern crate aoc_runner_derive;
 
-pub mod day1;
-pub mod day2;
-pub mod day3;
+pub mod day01;
+pub mod day02;
+pub mod day03;
 
 aoc_lib! { year = 2020 }
+
+pub static YEAR: u32 = aoc_factory::YEAR;
