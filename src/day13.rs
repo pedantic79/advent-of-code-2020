3a498b43641b2742243aee30a664e885cc24f01e
Dan Nguyen <pedantic@gmail.com>
2020-12-15 14:57:41 -0600

    Update day13 part2 comment

--- src/day13.rs
+++ src/day13.rs
@@ -51,49 +51,54 @@ pub fn part2(schedule: &BusSchedule) -> usize {
     Let's take the input to be: x,17,13,19
 
     Our first bus has frequency 17, with an offset of 1.
-    This means that the sum for this number is 16 to have the congruence be true
-    (sum + offset) % frequency == 0 i.e. (16 + 1) % 17 == 0
+    This means that the answer for this frequency is 16 to have the congruence be true
+    (x + offset) % frequency == 0 i.e. (16 + 1) % 17 == 0
 
-    This is done via a naïve modular inverse, where we step by 1 (step at this time).
+    This is done via a naïve modular inverse, where we step by 1.
+    e.g. for every number 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, etc.
+         we do (x + 1) % 17 and check if that is 0. The first value where that
+         is true is 16.
 
     Our second bus has frequency 13, with an offset of 2. For the intial congruence
     to remain true we need to step by 17.
 
     16, 33, 50, 67, etc.
 
-    We are also looking for the congruence to be. i.e (sum + 2) % 13 == 0
-    The first number were that is true is 50.
+    These numbers are all (number + 1) % 17 == 0.
+
+    Using this list of numbers, we need to find a number we need to find the first one
+    that the congruence to be. i.e (x + 2) % 13 == 0 is true. That number is 50.
+    (50 + 1) % 17 == 0 && (50 + 2) % 13 == 0
 
     Our third bus has frequency 19, with an offset of 3, For the first two congruence
-    to remain true, we need to step by lcm(17 * 13), so that this remains true.
+    to remain true, we need to step by 17 * 13, so that this remains true.
     forall.x, (x + 1) % 17 == 0 && (x + 2) % 13 == 0
-
 
     50, 271, 492, 713, 934, 1155, 1376, 1597, 1818, 2039, 2260, 2481, 2702, etc.
     See: https://play.rust-lang.org/?version=stable&mode=debug&edition=2018&gist=c831f5b1a07cbff938f718fdf1d6959d
 
-    we're searching for a number that is also (x + 3) % 19 == 0
+    We're searching for a number that is also (x + 3) % 19 == 0
 
-    Which is 2923, our answer.
+    Which is 2923, our answer. We can check that.
+
+    (2923 + 1) % 17 == 0 && (2923 + 2) % 13 == 0 && (2923 + 3) % 19 == 0
     */
 
     let mut step = 1;
-    let mut sum = 0;
+    let mut time = 0;
 
     for (offset, bl) in schedule.ids.iter().enumerate() {
         if let BusLine::ID(modulo) = bl {
-            while (sum + offset) % modulo != 0 {
-                // println!("sum: {}", sum);
-                sum += step;
+            while (time + offset) % modulo != 0 {
+                // println!("time: {}", time);
+                time += step;
             }
 
-            // Since all the numbers are co-prime, this isn't necessary
-            // step = step.lcm(bus_id);
             step *= modulo;
-            // println!("sum: {}, step: {}", sum, step);
+            // println!("sum: {}, step: {}", time, step);
         }
     }
-    sum
+    time
 }
 
 #[aoc(day13, part2, crt)]
