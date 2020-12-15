0739fbce38085411b3e8a80193efd05047a59218
Dan Nguyen <pedantic@gmail.com>
2020-12-15 15:40:23 -0600

    Update day15, faster implementation

--- src/day15.rs
+++ src/day15.rs
@@ -1,7 +1,15 @@
 use std::collections::HashMap;
 
+const PART1_ITERATIONS: usize = 2020;
+const PART2_ITERATIONS: usize = 30_000_000;
+
 #[aoc_generator(day15)]
 pub fn generator(input: &str) -> Vec<usize> {
+    input.split(',').map(|x| x.parse().unwrap()).collect()
+}
+
+#[aoc_generator(day15, part2, thirtytwo)]
+pub fn generator_32(input: &str) -> Vec<u32> {
     input.split(',').map(|x| x.parse().unwrap()).collect()
 }
 
@@ -22,9 +30,46 @@ pub fn solve(inputs: &[usize], limit: usize) -> usize {
         seen.entry(val)
             .and_modify(|(o, t)| {
                 *t = *o;
-                *o = i;
+                *o = i + 1;
             })
-            .or_insert((i, i));
+            .or_insert((i + 1, i + 1));
+    }
+
+    last
+}
+
+pub fn solve_alt(inputs: &[usize], limit: usize) -> usize {
+    let mut seen = vec![None; limit];
+    let mut last = 0;
+
+    for (i, t) in inputs.iter().copied().zip(1..) {
+        seen[i] = Some(t);
+        last = i;
+    }
+
+    for t in inputs.len()..limit {
+        let speak = seen[last].map_or(0, |n| t - n);
+        seen[last] = Some(t);
+        last = speak;
+    }
+
+    last
+}
+
+pub fn solve_32(inputs: &[u32], limit: u32) -> u32 {
+    let mut seen = vec![0; limit as usize];
+    let mut last = 0;
+
+    for (&i, t) in inputs.iter().zip(1..) {
+        seen[i as usize] = t;
+        last = i;
+    }
+
+    let l = inputs.len() as u32;
+    for i in l..limit {
+        let v = seen[last as usize];
+        seen[last as usize] = i;
+        last = if v == 0 { 0 } else { i - v };
     }
 
     last
@@ -32,12 +77,28 @@ pub fn solve(inputs: &[usize], limit: usize) -> usize {
 
 #[aoc(day15, part1)]
 pub fn part1(inputs: &[usize]) -> usize {
-    solve(inputs, 2020)
+    solve(inputs, PART1_ITERATIONS)
 }
 
+#[aoc(day15, part1, alt)]
+pub fn part1_alt(inputs: &[usize]) -> usize {
+    solve_alt(inputs, PART1_ITERATIONS)
+}
+
+#[cfg(slow)]
 #[aoc(day15, part2)]
 pub fn part2(inputs: &[usize]) -> usize {
-    solve(inputs, 30000000)
+    solve(inputs, PART2_ITERATIONS)
+}
+
+#[aoc(day15, part2, alt)]
+pub fn part2_alt(inputs: &[usize]) -> usize {
+    solve_alt(inputs, PART2_ITERATIONS)
+}
+
+#[aoc(day15, part2, thirtytwo)]
+pub fn part2_32(inputs: &[u32]) -> u32 {
+    solve_32(inputs, PART2_ITERATIONS as u32)
 }
 
 #[cfg(test)]
@@ -60,21 +121,21 @@ mod tests {
 
     #[test]
     pub fn test2() {
-        assert_eq!(part2(&generator(SAMPLE)), 175594);
+        assert_eq!(part2_32(&generator_32(SAMPLE)), 175594);
     }
 
     mod regression {
         use super::*;
 
         const INPUT: &str = include_str!("../input/2020/day15.txt");
-        const ANSWERS: (usize, usize) = (1618, 548531);
+        const ANSWERS: (usize, u32) = (1618, 548531);
 
         #[test]
         pub fn test() {
             let input = INPUT.trim_end_matches('\n'); // Trims trailing newline
 
-            assert_eq!(part1(&generator(input)), ANSWERS.0);
-            assert_eq!(part2(&generator(input)), ANSWERS.1);
+            assert_eq!(part1_alt(&generator(input)), ANSWERS.0);
+            assert_eq!(part2_32(&generator_32(input)), ANSWERS.1);
         }
     }
 }
