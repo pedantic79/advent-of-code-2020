981491f28a41ed09515e6aa8aee70ebbe9f97aa0
Dan Nguyen <pedantic@gmail.com>
2020-12-23 01:57:58 -0600

    Add day23 part2

--- src/day23.rs
+++ src/day23.rs
@@ -1,4 +1,5 @@
-use std::time::Instant;
+use std::collections::HashMap;
+use std::fmt::Write;
 
 #[derive(Debug, PartialEq)]
 pub struct Cups {}
@@ -11,57 +12,44 @@ pub fn generator(input: &str) -> Vec<usize> {
         .collect()
 }
 
+fn print(start: usize, ring: &HashMap<usize, usize>) -> String {
+    let mut output = String::new();
+    write!(output, "[{},", start).unwrap();
+    let mut i = ring[&start];
+
+    while i != start {
+        write!(output, " {},", i).unwrap();
+
+        i = ring[&i];
+    }
+
+    write!(output, "]").unwrap();
+    output
+}
+
 #[aoc(day23, part1)]
 pub fn part1(inputs: &[usize]) -> String {
     let len = inputs.len();
-    let mut cups = inputs.to_vec();
+    let mut ring = HashMap::new();
 
-    // let mut pos = 0;
-    let mut last = None;
+    for w in inputs.windows(2) {
+        ring.insert(w[0], w[1]);
+    }
+    ring.insert(inputs[len - 1], inputs[0]);
 
-    for mv in 0.. {
-        println!("\n-- move {} --", mv + 1);
-        let (mut pos, current) = if let Some(l) = last {
-            // println!("searching for {}", l);
-            let l_pos = cups.iter().position(|&x| x == l).unwrap();
+    println!("{}", print(inputs[0], &ring));
 
-            last = Some(cups[(l_pos + 1) % len]);
-            (l_pos + 1, last.unwrap())
-        } else {
-            last = Some(cups[0]);
-            (0, cups[0])
-        };
+    let mut current = inputs[0];
 
-        println!("cups ({}) {:?}", current, cups);
+    for _mv in 0..100 {
+        // println!("\n-- move {} --", _mv + 1);
+        // println!("cups ({}) {}", current, print(current, &ring));
 
-        if mv == 100 {
-            let current_pos = cups.iter().position(|&x| x == 1).unwrap();
-            // cups.rotate_left(current_pos);
-
-            let mut output = String::new();
-
-            for i in 1..len {
-                output
-                    .push(std::char::from_digit(cups[(current_pos + i) % len] as u32, 10).unwrap());
-            }
-
-            return output;
-        }
-
-        if pos + 4 > len {
-            cups.rotate_left(4);
-            pos -= 4;
-        }
-
-        let mut iter = cups.drain((pos + 1)..(pos + 4));
-
-        let three_cups = [
-            iter.next().unwrap(),
-            iter.next().unwrap(),
-            iter.next().unwrap(),
-        ];
-        drop(iter);
-        println!("picks up: {:?}", three_cups);
+        let mut three_cups = [0; 3];
+        three_cups[0] = ring[&current];
+        three_cups[1] = ring[&three_cups[0]];
+        three_cups[2] = ring[&three_cups[1]];
+        // println!("picks up: {:?}", three_cups);
 
         let mut dest = current - 1;
         if dest == 0 {
@@ -75,87 +63,53 @@ pub fn part1(inputs: &[usize]) -> String {
                 dest = len;
             }
         }
-        println!("destination: {}", dest);
-        let dest_index = cups.iter().position(|&x| x == dest).unwrap();
+        // println!("destination: {}", dest);
 
-        if mv == 3 {
-            println!("{:?}", cups);
-            println!("inserting at pos: {}", dest_index);
-        }
+        let next_current = ring[&three_cups[2]];
+        let dest_next = ring[&dest];
 
-        cups.insert((dest_index + 1) % (cups.len() + 1), three_cups[0]);
-        if mv == 3 {
-            println!("{:?}", cups);
-        }
-        cups.insert((dest_index + 2) % (cups.len() + 1), three_cups[1]);
-        if mv == 3 {
-            println!("{:?}", cups);
-        }
-        cups.insert((dest_index + 3) % (cups.len() + 1), three_cups[2]);
-        if mv == 3 {
-            println!("{:?}", cups);
-        }
+        ring.insert(current, next_current);
+        ring.insert(dest, three_cups[0]);
+        ring.insert(three_cups[2], dest_next);
+        current = next_current;
     }
 
-    unreachable!()
+    let mut current_pos = ring[&1];
+    let mut output = String::new();
+
+    while current_pos != 1 {
+        output.push(std::char::from_digit(current_pos as u32, 10).unwrap());
+        current_pos = ring[&current_pos];
+    }
+
+    output
 }
 
 #[aoc(day23, part2)]
-pub fn part2(inputs: &[usize]) -> String {
+pub fn part2(inputs: &[usize]) -> usize {
     const LEN: usize = 1_000_000;
-    let mut cups = inputs
-        .iter()
-        .copied()
-        .chain((10..=1_000_000).into_iter())
-        .collect::<Vec<_>>();
+    let mut ring = HashMap::new();
 
-    // let mut pos = 0;
-    let mut last = None;
+    for w in inputs.windows(2) {
+        ring.insert(w[0], w[1]);
+    }
+    ring.insert(inputs[inputs.len() - 1], 10);
+    ring.insert(LEN, inputs[0]);
 
-    let start = Instant::now();
-    for mv in 0.. {
-        if mv % 1024 == 0 {
-            println!(
-                "\n-- move {} {:?} --",
-                mv + 1,
-                Instant::now().duration_since(start)
-            );
-        }
-        let (mut pos, current) = if let Some(l) = last {
-            // println!("searching for {}", l);
-            let l_pos = cups.iter().position(|&x| x == l).unwrap();
+    for i in 10..LEN {
+        ring.insert(i, i + 1);
+    }
 
-            last = Some(cups[(l_pos + 1) % LEN]);
-            (l_pos + 1, last.unwrap())
-        } else {
-            last = Some(cups[0]);
-            (0, cups[0])
-        };
+    let mut current = inputs[0];
 
-        // println!("cups ({}) {:?}", current, cups);
+    for _mv in 0..10_000_000 {
+        // println!("\n-- move {} --", _mv + 1);
+        // println!("cups ({}) {}", current, print(current, &ring));
 
-        if mv == 10_000_000 {
-            let current_pos = cups.iter().position(|&x| x == 1).unwrap();
-
-            let mut output = String::new();
-            output.push_str(&cups[(current_pos + 1) % LEN].to_string());
-
-            return output;
-        }
-
-        if pos + 4 > LEN {
-            cups.rotate_left(4);
-            pos -= 4;
-        }
-
-        let mut iter = cups.drain((pos + 1)..(pos + 4));
-
-        let three_cups = [
-            iter.next().unwrap(),
-            iter.next().unwrap(),
-            iter.next().unwrap(),
-        ];
-        drop(iter);
+        let mut three_cups = [0; 3];
+        three_cups[0] = ring[&current];
+        three_cups[1] = ring[&three_cups[0]];
+        three_cups[2] = ring[&three_cups[1]];
         // println!("picks up: {:?}", three_cups);
 
         let mut dest = current - 1;
@@ -171,14 +125,20 @@ pub fn part2(inputs: &[usize]) -> String {
             }
         }
         // println!("destination: {}", dest);
-        let dest_index = cups.iter().position(|&x| x == dest).unwrap();
 
-        cups.insert((dest_index + 1) % (cups.len() + 1), three_cups[0]);
-        cups.insert((dest_index + 2) % (cups.len() + 1), three_cups[1]);
-        cups.insert((dest_index + 3) % (cups.len() + 1), three_cups[2]);
+        let next_current = ring[&three_cups[2]];
+        let dest_next = ring[&dest];
+
+        ring.insert(current, next_current);
+        ring.insert(dest, three_cups[0]);
+        ring.insert(three_cups[2], dest_next);
+        current = next_current;
     }
 
-    unreachable!()
+    let a = ring[&1];
+    let b = ring[&a];
+
+    a * b
 }
 
 #[cfg(test)]
@@ -201,23 +161,22 @@ mod tests {
     }
 
     #[test]
-    #[ignore]
     pub fn test2() {
-        assert_eq!(part2(&generator(SAMPLE)), "149245887792");
+        assert_eq!(part2(&generator(SAMPLE)), 149245887792);
     }
 
     mod regression {
         use super::*;
 
         const INPUT: &str = include_str!("../input/2020/day23.txt");
-        const ANSWERS: (usize, usize) = (32658947, 0);
+        const ANSWERS: (&str, usize) = ("32658947", 683486010900);
 
         #[test]
         pub fn test() {
             let input = INPUT.trim_end_matches('\n'); // Trims trailing newline
 
-            // assert_eq!(part1(&generator(input)), ANSWERS.0);
-            // assert_eq!(part2(&generator(input)), ANSWERS.1);
+            assert_eq!(part1(&generator(input)), ANSWERS.0);
+            assert_eq!(part2(&generator(input)), ANSWERS.1);
         }
     }
 }
