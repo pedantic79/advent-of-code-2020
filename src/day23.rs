141fdb13631b8a181192090037151ffb635f0b66
Dan Nguyen <pedantic@gmail.com>
2020-12-23 09:06:09 -0600

    Refactor to be generic

--- src/day23.rs
+++ src/day23.rs
@@ -1,78 +1,80 @@
-use std::collections::HashMap;
-use std::fmt::Write;
-
-#[derive(Debug, PartialEq)]
-pub struct Cups {}
+use std::{
+    convert::{TryFrom, TryInto},
+    fmt::{Debug, Write},
+};
 
 #[aoc_generator(day23)]
-pub fn generator(input: &str) -> Vec<usize> {
+pub fn generator(input: &str) -> Vec<u32> {
     input
         .chars()
-        .map(|x| x.to_digit(10).unwrap() as usize)
+        .map(|x| x.to_digit(10).unwrap() as u32)
         .collect()
 }
 
 #[allow(dead_code)]
-fn print(start: usize, ring: &HashMap<usize, usize>) -> String {
+fn to_string<T: num::PrimInt + Debug>(start: T, ring: &[T]) -> String {
     let mut output = String::new();
-    write!(output, "[({})", start).unwrap();
-    let mut i = ring[&start];
+    write!(output, "[({:?})", start).unwrap();
+    let mut i = ring[start.to_usize().unwrap()];
 
     while i != start {
-        write!(output, ", {}", i).unwrap();
+        write!(output, ", {:?}", i).unwrap();
 
-        i = ring[&i];
+        i = ring[i.to_usize().unwrap()];
     }
 
     write!(output, "]").unwrap();
     output
 }
 
-fn solve(ring: &mut Vec<usize>, mut current: usize, iteratations: usize) {
+fn solve<T>(ring: &mut [T], mut current: T, iteratations: usize)
+where
+    T: num::PrimInt + Debug,
+{
     let len = ring.len() - 1;
 
     for _mv in 0..iteratations {
         // println!("\n-- move {} --", _mv + 1);
-        // println!("cups {}", print(current, &ring));
+        // println!("cups {}", to_string(current, &ring));
 
-        let mut three_cups = [0; 3];
-        three_cups[0] = ring[current];
-        three_cups[1] = ring[three_cups[0]];
-        three_cups[2] = ring[three_cups[1]];
+        let mut three_cups = [T::zero(); 3];
+        three_cups[0] = ring[current.to_usize().unwrap()];
+        three_cups[1] = ring[three_cups[0].to_usize().unwrap()];
+        three_cups[2] = ring[three_cups[1].to_usize().unwrap()];
         // println!("picks up: {:?}", three_cups);
 
-        let mut dest = current - 1;
-        if dest == 0 {
-            dest = len;
+        let mut dest = current - T::one();
+        if dest == T::zero() {
+            dest = T::from(len).unwrap();
         }
 
         while three_cups.contains(&dest) {
-            dest -= 1;
+            dest = dest - T::one();
 
-            if dest == 0 {
-                dest = len;
+            if dest == T::zero() {
+                dest = T::from(len).unwrap();
             }
         }
-        // println!("destination: {}", dest);
+        // println!("destination: {:?}", dest);
 
-        let next_current = ring[three_cups[2]];
-        let dest_next = ring[dest];
-        ring[current] = next_current;
-        ring[dest] = three_cups[0];
-        ring[three_cups[2]] = dest_next;
+        let next_current = ring[three_cups[2].to_usize().unwrap()];
+        let dest_next: T = ring[dest.to_usize().unwrap()];
+        ring[current.to_usize().unwrap()] = next_current;
+        ring[dest.to_usize().unwrap()] = three_cups[0];
+        ring[three_cups[2].to_usize().unwrap()] = dest_next;
 
         current = next_current;
     }
 }
 
 #[aoc(day23, part1)]
-pub fn part1(inputs: &[usize]) -> String {
+pub fn part1(inputs: &[u32]) -> String {
     let mut ring = vec![0; inputs.len() + 1];
 
     for w in inputs.windows(2) {
-        ring[w[0]] = w[1];
+        ring[usize::try_from(w[0]).unwrap()] = w[1];
     }
-    ring[inputs[inputs.len() - 1]] = inputs[0];
+    ring[usize::try_from(inputs[inputs.len() - 1]).unwrap()] = inputs[0];
 
     solve(&mut ring, inputs[0], 100);
 
@@ -81,33 +83,33 @@ pub fn part1(inputs: &[usize]) -> String {
     let mut current_pos = ring[1];
     while current_pos != 1 {
         output.push(std::char::from_digit(current_pos as u32, 10).unwrap());
-        current_pos = ring[current_pos];
+        current_pos = ring[usize::try_from(current_pos).unwrap()];
     }
 
     output
 }
 
 #[aoc(day23, part2)]
-pub fn part2(inputs: &[usize]) -> usize {
+pub fn part2(inputs: &[u32]) -> u64 {
     const LEN: usize = 1_000_000;
     let mut ring = vec![0; LEN + 1];
 
     for w in inputs.windows(2) {
-        ring[w[0]] = w[1];
+        ring[usize::try_from(w[0]).unwrap()] = w[1];
     }
 
-    ring[inputs[inputs.len() - 1]] = 10;
+    ring[usize::try_from(inputs[inputs.len() - 1]).unwrap()] = 10;
     for (i, r) in ring.iter_mut().enumerate().skip(10) {
-        *r = i + 1;
+        *r = (i + 1).try_into().unwrap();
     }
     ring[LEN] = inputs[0];
 
     solve(&mut ring, inputs[0], 10_000_000);
 
     let a = ring[1];
-    let b = ring[a];
+    let b = ring[usize::try_from(a).unwrap()];
 
-    a * b
+    u64::from(a) * u64::from(b)
 }
 
 #[cfg(test)]
@@ -139,7 +141,7 @@ mod tests {
         use super::*;
 
         const INPUT: &str = include_str!("../input/2020/day23.txt");
-        const ANSWERS: (&str, usize) = ("32658947", 683486010900);
+        const ANSWERS: (&str, u64) = ("32658947", 683486010900);
 
         #[test]
         // #[ignore]
