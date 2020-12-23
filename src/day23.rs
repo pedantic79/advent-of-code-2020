8f79c7c1b211deea70fa251d862c090504a1d492
Dan Nguyen <pedantic@gmail.com>
2020-12-23 02:12:08 -0600

    Refactor day23

--- src/day23.rs
+++ src/day23.rs
@@ -12,13 +12,14 @@ pub fn generator(input: &str) -> Vec<usize> {
         .collect()
 }
 
+#[allow(dead_code)]
 fn print(start: usize, ring: &HashMap<usize, usize>) -> String {
     let mut output = String::new();
-    write!(output, "[{},", start).unwrap();
+    write!(output, "[({})", start).unwrap();
     let mut i = ring[&start];
 
     while i != start {
-        write!(output, " {},", i).unwrap();
+        write!(output, ", {}", i).unwrap();
 
         i = ring[&i];
     }
@@ -27,23 +28,12 @@ fn print(start: usize, ring: &HashMap<usize, usize>) -> String {
     output
 }
 
-#[aoc(day23, part1)]
-pub fn part1(inputs: &[usize]) -> String {
-    let len = inputs.len();
-    let mut ring = HashMap::new();
+fn solve(ring: &mut HashMap<usize, usize>, mut current: usize, iteratations: usize) {
+    let len = ring.len();
 
-    for w in inputs.windows(2) {
-        ring.insert(w[0], w[1]);
-    }
-    ring.insert(inputs[len - 1], inputs[0]);
-
-    println!("{}", print(inputs[0], &ring));
-
-    let mut current = inputs[0];
-
-    for _mv in 0..100 {
+    for _mv in 0..iteratations {
         // println!("\n-- move {} --", _mv + 1);
-        // println!("cups ({}) {}", current, print(current, &ring));
+        // println!("cups {}", print(current, &ring));
 
         let mut three_cups = [0; 3];
         three_cups[0] = ring[&current];
@@ -73,10 +63,22 @@ pub fn part1(inputs: &[usize]) -> String {
         ring.insert(three_cups[2], dest_next);
         current = next_current;
     }
+}
 
-    let mut current_pos = ring[&1];
+#[aoc(day23, part1)]
+pub fn part1(inputs: &[usize]) -> String {
+    let mut ring = HashMap::new();
+
+    for w in inputs.windows(2) {
+        ring.insert(w[0], w[1]);
+    }
+    ring.insert(inputs[inputs.len() - 1], inputs[0]);
+
+    solve(&mut ring, inputs[0], 100);
+
     let mut output = String::new();
 
+    let mut current_pos = ring[&1];
     while current_pos != 1 {
         output.push(std::char::from_digit(current_pos as u32, 10).unwrap());
         current_pos = ring[&current_pos];
@@ -100,40 +102,7 @@ pub fn part2(inputs: &[usize]) -> usize {
         ring.insert(i, i + 1);
     }
 
-    let mut current = inputs[0];
-
-    for _mv in 0..10_000_000 {
-        // println!("\n-- move {} --", _mv + 1);
-        // println!("cups ({}) {}", current, print(current, &ring));
-
-        let mut three_cups = [0; 3];
-        three_cups[0] = ring[&current];
-        three_cups[1] = ring[&three_cups[0]];
-        three_cups[2] = ring[&three_cups[1]];
-        // println!("picks up: {:?}", three_cups);
-
-        let mut dest = current - 1;
-        if dest == 0 {
-            dest = LEN;
-        }
-
-        while three_cups.contains(&dest) {
-            dest -= 1;
-
-            if dest == 0 {
-                dest = LEN;
-            }
-        }
-        // println!("destination: {}", dest);
-
-        let next_current = ring[&three_cups[2]];
-        let dest_next = ring[&dest];
-
-        ring.insert(current, next_current);
-        ring.insert(dest, three_cups[0]);
-        ring.insert(three_cups[2], dest_next);
-        current = next_current;
-    }
+    solve(&mut ring, inputs[0], 10_000_000);
 
     let a = ring[&1];
     let b = ring[&a];
@@ -161,6 +130,7 @@ mod tests {
     }
 
     #[test]
+    // #[ignore]
     pub fn test2() {
         assert_eq!(part2(&generator(SAMPLE)), 149245887792);
     }
@@ -172,10 +142,18 @@ mod tests {
         const ANSWERS: (&str, usize) = ("32658947", 683486010900);
 
         #[test]
-        pub fn test() {
+        // #[ignore]
+        pub fn test_1() {
             let input = INPUT.trim_end_matches('\n'); // Trims trailing newline
 
             assert_eq!(part1(&generator(input)), ANSWERS.0);
+        }
+
+        #[test]
+        // #[ignore]
+        pub fn test_2() {
+            let input = INPUT.trim_end_matches('\n'); // Trims trailing newline
+
             assert_eq!(part2(&generator(input)), ANSWERS.1);
         }
     }
