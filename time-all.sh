436384771642812cd1efa2f5db8bfa689399da31
Dan Nguyen <pedantic@gmail.com>
2025-01-25 17:02:26 -0600

    Common: move justfile to common

--- time-all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ -n "$1" ]; then
-    tests=$(printf "day%02d::tests::regression" $1)
-else
-    tests="regression"
-fi
-
-hyperfine --warmup 2 "cargo test --release --quiet --lib -- --test-threads=1 $tests"
